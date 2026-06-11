9cec1f5af6154c856e6c651f36f998c945bd2d63
McDentalPulse-app <mcdentalpulse@outlook.com>
2026-06-11 16:10:01 -0600

    Conectar reportes permisos y vacaciones con Firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDCKbMZKp46Dn0QGr5KmjFtrbCH3H6LeEc",
+  authDomain: "mcdental-pulse.firebaseapp.com",
+  projectId: "mcdental-pulse",
+  storageBucket: "mcdental-pulse.firebasestorage.app",
+  messagingSenderId: "181118152189",
+  appId: "1:181118152189:web:e99c719584322b081abd94"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
